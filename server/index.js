8e593e66d457246ece51394998850eb889b8c007
unknown <andre.lb865854@gmail.com>
2025-11-27 13:54:56 -0300

    likes

--- server/index.js
+++ server/index.js
@@ -14,9 +14,11 @@ const commentsRouter = require("./routes/Comments");
 app.use("/comments", commentsRouter);
 const usersRouter = require("./routes/Users");
 app.use("/auth", usersRouter);
+const likesRouter = require("./routes/Likes");
+app.use("/likes", likesRouter);
 
 db.sequelize.sync().then(() => {
     app.listen(3001, () => {
-        console.log("Servidor rodando na porta 3001");
+        console.log("Server running on port 3001");
     });
 });
